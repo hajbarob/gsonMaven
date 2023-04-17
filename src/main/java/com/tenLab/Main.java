b94d02c7858ed67448dd849cda2f34bb65eaf57c
vokh <haiba.vova@gmail.com>
2023-04-17 19:44:20 +0300

    Initial commit

---
+++ src/main/java/com/tenLab/Main.java
@@ -0,0 +1,11 @@
+package com.tenLab;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    }
+}
